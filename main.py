b63310fa9837c97e09f78f1ca0f0fa90a62f3622
rohitsabu08 <rohitsabu96@gmail.com>
2021-09-29 14:29:27 +0530

    docker p2

--- main.py
+++ main.py
@@ -40,4 +40,4 @@ def get_req_parameters():
     
 if __name__ == "__main__":
     #port = int(os.environ.get('PORT',5000))
-    app.run(host= "0.0.0.0",port =port)
+    app.run(host= "0.0.0.0",port =3400)
